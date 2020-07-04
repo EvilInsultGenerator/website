6309488ca02dc47809f55d15e4844ac9512f049b
Evil Insult Generator <marvin@evilinsult.com>
2020-07-04 20:25:49 +0200

    Create db.conf.php

---
+++ db.conf.php
@@ -0,0 +1,17 @@
+<?php
+$dbconfig['host'] = 'localhost';
+$dbconfig['user'] = 'YOURDBUSER';
+$dbconfig['base'] = 'YOURDB';
+$dbconfig['pass'] = 'YOURDBPASS';
+$dbconfig['char'] = 'utf8';
+$active_insult = 1;
+$sql_insult = "SELECT * FROM insults WHERE number >= RAND() * (SELECT MAX(number) FROM insults WHERE active = :active and language = :lang ) AND language = :lang AND active = :active ORDER BY number LIMIT 1";
+$update_counter = "UPDATE insults SET shown = shown + 1 WHERE number = :number";
+
+
+try {
+    $pdo = new PDO('mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['base'] . ';charset=' . $dbconfig['char'] . ';', $dbconfig['user'], $dbconfig['pass']);
+} catch (PDOException $e) {
+    exit('Whoops. Unable to connect Database.');
+}
+?>
