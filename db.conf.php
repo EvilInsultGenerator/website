28eeca120873e774cab518d8933c3dd960e34666
Jason Harris <jasonharris306@gmail.com>
2020-11-30 10:21:38 +0000

    Modify $sql_insult to reference column names

--- db.conf.php
+++ db.conf.php
@@ -5,7 +5,23 @@ $dbconfig['base'] = 'YOURDB';
 $dbconfig['pass'] = 'YOURDBPASS';
 $dbconfig['char'] = 'utf8';
 $active_insult = 1;
-$sql_insult = "SELECT * FROM insults WHERE number >= RAND() * (SELECT MAX(number) FROM insults WHERE active = :active and language = :lang ) AND language = :lang AND active = :active ORDER BY number LIMIT 1";
+
+$sql_insult = "SELECT 
+    number,
+    language,
+    insult,
+    created,
+    shown,
+    createdby,
+    active,
+    comment
+FROM insults 
+WHERE number >= RAND() * (SELECT MAX(number) FROM insults WHERE active = :active and language = :lang ) 
+    AND language = :lang 
+    AND active = :active
+ORDER BY number LIMIT 1;
+";
+
 $update_counter = "UPDATE insults SET shown = shown + 1 WHERE number = :number";
 
 
