bbd765d8bd800bf50a968dee6db85687fc78413d
Evil Insult Generator <marvin@evilinsult.com>
2022-06-12 20:38:22 +0200

    Update db.conf.php

--- db.conf.php
+++ db.conf.php
@@ -8,7 +8,6 @@ $active_insult = 1;
 $sql_insult = "SELECT * FROM insults WHERE language= :lang AND active = :active ORDER BY RAND() LIMIT 1";
 $update_counter = "UPDATE insults SET shown = shown + 1 WHERE number = :number";
 
-
 try {
     $pdo = new PDO('mysql:host=' . $dbconfig['host'] . ';dbname=' . $dbconfig['base'] . ';charset=' . $dbconfig['char'] . ';', $dbconfig['user'], $dbconfig['pass']);
 } catch (PDOException $e) {
